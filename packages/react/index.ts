22d352f2eefeb30f640f9ada95791148f5919f7e
maxingyuan <mxydl2009@163.com>
2024-05-07 15:01:48 +0800

    fix: fix some type definition in react hook

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -12,7 +12,7 @@ export const useState: Dispatcher['useState'] = (initialState: any) => {
 };
 
 export const __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED = {
-	ReactCurrentDispatcher: currentDispatcher
+	currentDispatcher
 };
 
 export default {
--- packages/react/src/currentDispatcher.ts
+++ packages/react/src/currentDispatcher.ts
@@ -2,11 +2,10 @@ import { Action } from 'packages/shared/ReactTypes';
 
 // Dispatcher是所有的Hooks集合，在源码里，hook都是dispatch函数
 export interface Dispatcher {
-	useState: <T>(initialState: T | (() => T)) => [T, Dispatch];
-	useEffect: any;
+	useState: <T>(initialState: T | (() => T)) => [T, Dispatch<T>];
 }
 
-export type Dispatch = <T>(action: Action<T>) => void;
+export type Dispatch<T> = (action: Action<T>) => void;
 
 // 当前使用的Hooks集合， 用current属性指向当前Hooks集合
 export const currentDispatcher: { current: Dispatcher | null } = {
